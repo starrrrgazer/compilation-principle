39d3ab921a88d9497ab73d01b0ed72ab7ace4733
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-10-06 22:48:52 +0800

    pre

--- src/Lexer.java
+++ src/Lexer.java
@@ -2,112 +2,113 @@ import java.util.Scanner;
 
 public class Lexer {
     String[] tokenString = {
-            "=", ";", "(", ")", "{", "}", "+", "*", "/", "<", ">","==",
+            "=", ";", "(", ")", "{", "}", "+", "*", "/", "<", ">", "==",
             "if", "else", "while", "break", "continue", "return"
-            };
-    char[] tokenChar = {'=', ';', '(', ')', '{' , '}', '+', '*', '/', '<', '>'};
+    };
+    char[] tokenChar = {'=', ';', '(', ')', '{', '}', '+', '*', '/', '<', '>'};
     String[] output = {
             "Assign", "Semicolon", "LPar", "RPar", "LBrace", "RBrace", "Plus", "Mult", "Div", "Lt", "Gt", "Eq",
             "If", "Else", "While", "Break", "Continue", "Return",
             "Err"};
+
     Boolean isDigit(String str) {
         return str.matches("^[0-9]+$");
     }
+
     Boolean isIdentifier(String str) {
         return str.matches("^[a-zA-Z0-9_]+$");
     }
-    void printIdent(String str){
+
+    void printIdent(String str) {
         System.out.println("Ident(" + str + ")");
     }
-    void printNumber(String str){
+
+    void printNumber(String str) {
         System.out.println("Number(" + str + ")");
     }
-    void printToken(int index){
+
+    void printToken(int index) {
         System.out.println(output[index]);
-    };
-    int getIndexOfStringArr(String str, String[] strings){
+    }
+
+    ;
+
+    int getIndexOfStringArr(String str, String[] strings) {
         int len = strings.length;
-        for(int i = 0; i < len; i++){
-            if(strings[i].equals(str)){
+        for (int i = 0; i < len; i++) {
+            if (strings[i].equals(str)) {
                 return i;
             }
         }
         return -1;
     }
-    int getIndexOfCharArr(char c, char[] chars){
+
+    int getIndexOfCharArr(char c, char[] chars) {
         int len = chars.length;
-        for(int i = 0; i < len; i++){
-            if(c == chars[i]){
+        for (int i = 0; i < len; i++) {
+            if (c == chars[i]) {
                 return i;
             }
         }
         return -1;
     }
-    void lexAnalyze(String str){
+
+    void lexAnalyze(String str) {
         int index = getIndexOfStringArr(str, tokenString);
-        if(index != -1){
+        if (index != -1) {
             printToken(index);
             return;
         }
         char[] chars = str.toCharArray();
         int len = str.length();
-        for(int i = 0;i < len; i++){
-            int index1 = getIndexOfCharArr(chars[i],tokenChar);
+        for (int i = 0; i < len; i++) {
+            int index1 = getIndexOfCharArr(chars[i], tokenChar);
 //            System.out.println(str + " index:" + index + " index1:" + index1);
-            if(index1 > 0){
+            if (index1 > 0) {
                 printToken(index1);
-            }
-            else if(index1 == 0){
+            } else if (index1 == 0) {
 
-                if(i+1<len && chars[i+1] == '='){
+                if (i + 1 < len && chars[i + 1] == '=') {
                     System.out.println("Eq");
                     i++;
-                }
-
-                else{
+                } else {
                     printToken(index1);
                 }
-            }
-
-            else if(Character.isDigit(chars[i])){
-                int j=i+1;
-                while(j<len && Character.isDigit(chars[j])){
+            } else if (Character.isDigit(chars[i])) {
+                int j = i + 1;
+                while (j < len && Character.isDigit(chars[j])) {
                     j++;
                 }
-                printNumber(str.substring(i,j));
-                i=j-1;
-            }
-
-            else if(Character.isLetter(chars[i]) ||  chars[i] == '_'){
-                int j=i+1;
-                while(j<len && (Character.isLetter(chars[j]) ||  chars[j] == '_' || Character.isDigit(chars[j]))){
+                printNumber(str.substring(i, j));
+                i = j - 1;
+            } else if (Character.isLetter(chars[i]) || chars[i] == '_') {
+                int j = i + 1;
+                while (j < len && (Character.isLetter(chars[j]) || chars[j] == '_' || Character.isDigit(chars[j]))) {
                     j++;
                 }
-                int index2 = getIndexOfStringArr(str.substring(i,j),tokenString);
+                int index2 = getIndexOfStringArr(str.substring(i, j), tokenString);
 
-                if(index2 == -1){
-                    printIdent(str.substring(i,j));
-                }
-
-                else{
+                if (index2 == -1) {
+                    printIdent(str.substring(i, j));
+                } else {
                     printToken(index2);
                 }
-                i=j-1;
-            }
-            else{
+                i = j - 1;
+            } else {
                 System.out.println("Err");
                 System.exit(0);
             }
         }
     }
+
     public static void main(String[] args) {
         Lexer lexer = new Lexer();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        while( !input.isEmpty()){
+        while (!input.isEmpty()) {
             String[] inputArr = input.trim().split("\\s+");
             input = scanner.nextLine();
-            for(String str : inputArr){
+            for (String str : inputArr) {
                 lexer.lexAnalyze(str);
             }
         }
